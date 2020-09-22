9bbe3becac456b4e6ff5bb8bbcdf8130fdbd8a8e
Jorge Potosme <fedoral10@gmail.com>
2020-09-21 20:56:25 -0600

    Route para select game y bets

--- src/API/games.ts
+++ src/API/games.ts
@@ -22,3 +22,28 @@ export function getActiveGames() {
             console.log(error)
         })
 }
+
+export function getBetsByGame(gameId: string) {
+
+    const url = `${basePath}/user/bet/listBetsByGame`;
+
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            game: gameId
+        })
+    }
+    return fetch(url, params)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            return result.data;
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
--- src/pages/GamePage/index.tsx
+++ src/pages/GamePage/index.tsx
@@ -1,9 +1,23 @@
-import React, { Fragment, ReactElement } from 'react'
-
+import React, { Fragment, ReactElement,useEffect } from 'react'
+import { getBetsByGame } from '../../API/games'
 import Game from '../../components/GameComponent/MainComponent'
-
+import {
+    useParams
+} from "react-router-dom";
 
 const GamePage: React.FC = (): ReactElement => {
+    
+    let { gameId } = useParams<any>();
+
+    useEffect(()=>{
+        getBetsByGame(gameId).then(bets => {
+            console.log(bets)
+            bets?.map((item:any) => {
+                alert(JSON.stringify(item))
+            })
+        })
+    },[])
+
     return (
         <Fragment>
             <div className="GameSection">
